161642055e4f6b18a0ff2a74d7c4e433a392e59d
yjvijfhvk <yjvijfhvk@gmail.com>
2017-04-28 15:17:55 +0800

    thread

---
+++ zz_udp_class.py
@@ -0,0 +1,23 @@
+﻿import socket
+class ZZUdpClass(object):
+    def __init__(self,host,port,timeout):
+        self.host =host
+        self.port =port
+        self.timeout = timeout
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.server.settimeout(self.timeout)
+        self.server.bind((self.host, self.port))
+    def rev(self):
+        try:
+            data,addr= self.server.recvfrom(1024)
+            return data,addr
+        except socket.timeout:
+            print('Time is out. {0} seconds have passed'.format(self.timeout ))
+            return False
+    def send(self,data,addr):
+        try:
+            self.server.sendto(data,addr)
+        except Exception as err:
+            print("error")
+            print(err)
+            return False
